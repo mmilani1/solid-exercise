ac57d8837a6bb8c3cb6d4547f62352d8f91797cd
Matheus Milani <m_milani@hotmail.com>
2020-06-14 19:22:11 -0300

    Big Bang

---
+++ src/test/kotlin/br/com/solid/exercise/exercicio/SolidApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.solid.exercise.exercicio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SolidApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
